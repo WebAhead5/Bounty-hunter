ff525035f26a6306f06f32152a58ed13a1f1b21d
Marwanrizik91 <marwan.rizik@gmail.com>
2020-05-06 15:34:12 +0300

    deleted unncessary file

--- src/tests/draftTests.js
+++
@@ -1,9 +0,0 @@
-tape('test remove bounty', async t => {
-    id = 1;
-    await removeBounty(id);
-    let actual = db.query('SELECT id FROM bounties')
-    let expected = "I can't remember what this should give";
-    t.deepEqual(actual, expected)
-    t.end()
-
-})
