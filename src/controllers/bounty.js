629bc9d6c52f8280a78a8cebe0c554d587570f27
Jake <jakepowis@gmail.com>
2020-05-06 12:28:45 +0300

    adding try catch blocks for async func

--- src/controllers/bounty.js
+++ src/controllers/bounty.js
@@ -9,67 +9,93 @@ const getUserData = require('../models/queries/getUserData')
 
 //GET BOUNTY (IF lOGGED IN)
 exports.get = async (req, res) => {
-    if (res.locals.error) {
-        return res.render('error', {
-            error: res.locals.error
-        });
+    try {
+        if (res.locals.error) {
+            return res.render('error', {
+                error: res.locals.error
+            });
+        }
+
+        const bountyData = await getBountiesById(req.params.id);
+        const messageData = await getMessageData(req.params.id);
+        //const userData = await getUserData(req.params.id);
+        //console.log('req.params.id: ',req.params.id);
+
+        //console.log('userData: ',userData);
+        // let usernames = userData.reduce(
+        //     (acc, obj) => {
+        //   acc[obj.id] = obj.name
+        //     return acc;
+        //   },{})
+
+        // console.log('usernames: ', usernames);
+        // console.log('bountyData: ', bountyData);
+
+        if (res.locals.signedIn) {
+            return res.render('bounty', {
+                signedIn: true,
+                bounties: bountyData,
+                messages: messageData,
+                admin: res.locals.admin
+            });
+        }
+
+        //return to welcome page as default if not logged in
+        res.redirect('/');
     }
-
-    const bountyData = await getBountiesById(req.params.id);
-    const messageData = await getMessageData(req.params.id);
-    //const userData = await getUserData(req.params.id);
-    //console.log('req.params.id: ',req.params.id);
-    
-    //console.log('userData: ',userData);
-    // let usernames = userData.reduce(
-    //     (acc, obj) => {
-    //   acc[obj.id] = obj.name
-    //     return acc;
-    //   },{})
-    
-    // console.log('usernames: ', usernames);
-    // console.log('bountyData: ', bountyData);
-
-    if (res.locals.signedIn) {
-        return res.render('bounty', {
-            signedIn: true,
-            bounties: bountyData,
-            messages: messageData,
-            admin: res.locals.admin
+    catch (error) {
+        res.render('error', {
+            statusCode: "Messages",
+            errorMessage: error.message
         });
-    }
-
-    //return to welcome page as default if not logged in
-    res.redirect('/');
-};
+    };
+}
 
 exports.post = async (req, res) => {
-    if (res.locals.error) {
-        return res.render('error', {
-            error: res.locals.error
-        });
+    try {
+        if (res.locals.error) {
+            return res.render('error', {
+                error: res.locals.error
+            });
+        }
+
+        const { name, picture, crimes, bounty, status, furtherinfo } = req.body
+
+        if (res.locals.signedIn && res.locals.admin) {
+            await addBounty(name, picture, crimes, bounty, status, furtherinfo)
+            res.redirect('/')
+        }
     }
-
-    const { name, picture, crimes, bounty, status, furtherinfo } = req.body
-
-    if (res.locals.signedIn && res.locals.admin) {
-        await addBounty(name, picture, crimes, bounty, status, furtherinfo)
-        res.redirect('/')
+    catch (error) {
+        res.render('error', {
+            statusCode: " Bounty",
+            errorMessage: error.message
+        });
     }
 }
 
 exports.delete = async (req, res) => {
-    if (res.locals.error) {
-        return res.render('error', {
-            error: res.locals.error
-        });
+    try {
+        if (res.locals.error) {
+            return res.render('error', {
+                error: res.locals.error
+            });
+        }
+        const id = req.params.id
+        if (res.locals.signedIn && res.locals.admin) {
+            await removeBounty(id)
+            res.redirect('/')
+        }
     }
-    const id = req.params.id
-    if (res.locals.signedIn && res.locals.admin) {
-        await removeBounty(id)
-        res.redirect('/')
+    catch (error) {
+        res.render('error', {
+            statusCode: "Delete Bounty",
+            errorMessage: error.message
+        });
+
     }
 }
+
 
 exports.addBountyPage = async (req, res) => {
     if (res.locals.error) {
--- src/controllers/home.js
+++ src/controllers/home.js
@@ -2,26 +2,35 @@ const getBounties = require('../models/queries/readBounties');
 
 //GET HOMEPAGE (IF lOGGED IN)
 exports.get = async (req, res) => {
-    console.log('home requested')
-    if (res.locals.error) {
-        return res.render('error', {
-            error: res.locals.error
-        });
-    } else {
-
-        const bountyData = await getBounties()
-        if (res.locals.signedIn) {
-            return res.render('home', {
-                signedIn: true,
-                bounties: bountyData,
-                username: res.locals.username,
-                admin: res.locals.admin
+    try {
+        console.log('home requested')
+        if (res.locals.error) {
+            return res.render('error', {
+                error: res.locals.error
             });
+        } else {
 
-        } else {    //return to welcome page as default
-            res.render('home', {
-                signedIn: false
-            });
+            const bountyData = await getBounties()
+            if (res.locals.signedIn) {
+                return res.render('home', {
+                    signedIn: true,
+                    bounties: bountyData,
+                    username: res.locals.username,
+                    admin: res.locals.admin
+                });
+
+            } else {    //return to welcome page as default
+                res.render('home', {
+                    signedIn: false
+                });
+            }
         }
+    }
+    catch (error) {
+        res.render('error', {
+            statusCode: "Home",
+            errorMessage: error.message
+        });
+
     }
 };
