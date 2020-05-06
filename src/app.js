30f7f03cbb7a4e9820d0b206e39aae1b3fa6c05f
James Daniels <jrd656@gmail.com>
2020-05-06 10:34:53 +0300

    favicon fix

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const cookieParser = require('cookie-parser')
+const favicon = require('serve-favicon')
 
 const controllers = require('./controllers/index')
 require('dotenv').config();
@@ -10,7 +11,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(cookieParser())
 
-app.use(favicon(path.join(__dirname, '.', 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, '..', 'public', 'favicon.ico')));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
